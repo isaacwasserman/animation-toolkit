d54d80bc5718be7173b7f1c7fa5c0c6710e39423
Isaac Wasserman <isaacrwasserman@gmail.com>
2021-09-13 23:03:00 -0400

    did unique

--- assignments/a1-hello/unique.cpp
+++ assignments/a1-hello/unique.cpp
@@ -2,19 +2,78 @@
 
 using namespace glm;
 
-class Tree {
+class Ball {
   public:
-    vec3 rootPosition;
-    vec3 tipPosition;
-    float length;
-    float width;
-    vec3 rotation;
-    Tree(const vec3& rootPos, const vec3& relativeTipPos, const float& wid) {
-      rootPosition = rootPos;
-      tipPosition = rootPos + relativeTipPos;
-      rotation = vec3(0,0,atan2(relativeTipPos[0], relativeTipPos[1]));
-      width = wid;
-      length = glm::length(relativeTipPos);
+    vec3 position;
+    float radius = 30;
+    vec3 color = vec3(1,1,1);
+    float dynamicsMultiplier = 20;
+    vec3 acceleration = vec3(0,-9.8,0) * dynamicsMultiplier;
+    vec3 velocity = vec3(0);
+    float bounciness = 0.95;
+    float nextAllowedCollisionTime = 0;
+    float collisionTimer = 0.25;
+
+    Ball(const vec3& pos, const vec3& vel, const vec3& col) {
+      position = pos;
+      velocity = vel;
+      color = col;
+    }
+    
+    void updateDynamics(const float& dt, const int& width, const int& height, std::vector<Ball>& balls, const float& elapsedTime){
+      position += velocity * dt;
+      velocity += acceleration * dt;
+      checkBoxCollision(width, height);
+      // checkBallCollision(balls, elapsedTime);
+    }
+
+    int checkBoxCollision(const int& width, const int& height){
+      // Right collision
+      if(position[0] + (radius * 0.5) >= width && velocity[0] > 0){
+        velocity[0] = velocity[0] * -1 * bounciness;
+        return 1;
+      }
+      // Left collision
+      else if(position[0] - (radius * 0.5) <= 0 && velocity[0] < 0){
+        velocity[0] = velocity[0] * -1 * bounciness;
+        return 2;
+      }
+      // Top collision
+      else if(position[1] + (radius * 0.5) >= height && velocity[1] > 0){
+        velocity[1] = velocity[1] * -1 * bounciness;
+        return 3;
+      }
+      // Bottom collision
+      else if(position[1] - (radius * 0.5) <= 0 && velocity[1] < 0){\
+        velocity[1] = velocity[1] * -1 * bounciness;
+        return 4;
+      }
+      else {
+        return 0;
+      }
+    }
+
+    int checkBallCollision(std::vector<Ball>& balls, const float& elapsedTime){
+      float collisionBuffer = 30;
+      if(elapsedTime >= nextAllowedCollisionTime){
+        for(Ball& ball : balls){
+        if(&ball != this){
+          float angleOfApproach = atan2((position - ball.position)[0], (position - ball.position)[1]);
+          float velocityAngle = atan2(velocity[0], velocity[1]);
+          float distance = length(position - ball.position);
+          if(distance <= (radius * 2) + collisionBuffer && std::abs(angleOfApproach - velocityAngle) <= (pi<float>() / 2)){
+            float currentVelocityMagnitude = length(velocity);
+            float angleOfDeparture = angleOfApproach + pi<float>();
+            float newVelocityMagnitude = currentVelocityMagnitude * bounciness;
+            vec3 newVelocityUnitVector = vec3(cos(angleOfDeparture), sin(angleOfDeparture), 0);
+            velocity = newVelocityUnitVector * newVelocityMagnitude;
+            nextAllowedCollisionTime = elapsedTime + collisionTimer;
+            return 0;
+          }
+        }
+      }
+      }
+      return 0;
     }
 };
 
@@ -23,21 +82,106 @@ class Unique : public atkui::Framework {
   Unique() : atkui::Framework(atkui::Orthographic) {
   }
 
+  vec3 center = vec3(width()*0.5, height()*0.5, 0);
+  std::vector<Ball> balls;
+  int nBalls = 20;
+  float maxInitialXVelocity = 200;
+  float nextGravityChange = 10;
+
+  std::vector<vec3> palette = {
+    vec3(1,0.6824,0.0018),
+    vec3(0.996, 0.306, 0),
+    vec3(1, 0.059, 0.502),
+    vec3(0.945, 0.949, 0.933)
+  };
+
   virtual void setup() {
-    // your code here!
+    for(int i=0; i<nBalls; i++){
+      balls.push_back(Ball(randomBallPosition(), vec3((agl::random() * maxInitialXVelocity * 2)-maxInitialXVelocity, 0, 0), palette[rand() % palette.size()]));
+    }
   }
 
   virtual void scene() {
-    vec3 center = vec3(width()*0.5, height()*0.5, 0);
-    vec3 rootPos = vec3(width()*0.5, height()*0.25, 0);
-    vec3 tipPos = rootPos+vec3(-20,50,0);
-    Tree tree = Tree(rootPos, tipPos, 20);
-    setColor(vec3(1,1,1));
-    renderer.translate(vec3((rootPos[0]+tipPos[0])/2, (rootPos[1]+tipPos[1])/2, 0));
-    renderer.scale(vec3(tree.width, tree.length, 1));
-    renderer.rotate(1,vec3(20,0,0));
-    renderer.cube();
+    drawBackground();
+    for(Ball& ball : balls){
+      drawBall(ball);
+    }
+    if(elapsedTime() >= nextGravityChange){
+      // gravityShift();
+    }
   }
+
+  void drawBall(Ball& ball){
+    setColor(ball.color);
+    drawSphere(ball.position, ball.radius);
+    ball.updateDynamics(dt(), width(), height(), balls, elapsedTime());
+  }
+
+  void drawBackground(){
+    setColor(vec3(0.263, 0.314, 0.345));
+    renderer.push();
+    renderer.translate(center - vec3(0,0,100));
+    renderer.scale(vec3(width(), height(), 1));
+    renderer.cube();
+    renderer.pop();
+  }
+
+  bool ballOverlaps(const vec3 pos){
+    for(Ball& ball : balls){
+      float threshold = ball.radius + 30;
+      float distance = length(pos - ball.position);
+      if(distance <= threshold){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  vec3 randomBallPosition(){
+    vec3 pos = vec3(agl::random() * width(), agl::random() * height(), 0);
+    while(ballOverlaps(pos) && pos[1] > height() * 0.5){
+      pos = vec3(agl::random() * width(), agl::random() * height(), 0);
+    }
+    return pos;
+  }
+
+  float random(){
+    return ((float) rand()) / ((float) RAND_MAX);
+  }
+
+  void spawnBall(){
+    balls.push_back(Ball(vec3(random() * width(), random() * height(), 0), vec3((random() * maxInitialXVelocity * 2)-maxInitialXVelocity, 0, 0), palette[rand() % palette.size()]));
+  }
+
+  void gravityShift(){
+    int side = rand() % 4;
+    vec3 gravity;
+    float dynamicsMultiplier = balls[0].dynamicsMultiplier;
+    if(side == 0){
+      gravity = vec3(0,-9.8,0);
+    }
+    else if(side == 1){
+      gravity = vec3(-9.8,0,0);
+    }
+    else if(side == 2){
+      gravity = vec3(0,9.8,0);
+    }
+    else if(side == 3){
+      gravity = vec3(9.8,0,0);
+    }
+    for(Ball& ball : balls){
+      ball.acceleration = gravity * dynamicsMultiplier;
+    }
+    nextGravityChange += 10;
+  }
+
+  void keyUp(int key, int mods){
+      // Space pressed
+      if(key == GLFW_KEY_R){
+         balls = {};
+         setup();
+      }
+   }
 };
 
 int main(int argc, char** argv) {
