3bea5f1960f921e86956da5c82989d43fa086102
Isaac <Isaac Wasserman>
2021-11-04 13:13:07 -0400

    wip

--- assignments/a7-motion/butterfly.cpp
+++ assignments/a7-motion/butterfly.cpp
@@ -30,6 +30,7 @@ public:
 
    void scene() {
       Joint* body = skeleton.getByName("Body");
+      body->setLocalTranslation(vec3(100,cos(elapsedTime() + pi<float>()) * 50 + 200,100));
       Joint* lwing = skeleton.getByName("LWing");
       lwing->setLocalRotation(glm::angleAxis(sin(elapsedTime()), vec3(0,0,1)));
 
@@ -57,29 +58,66 @@ public:
          vec3(80,0,0), 
          vec3(120,20,120));
 
-      Transform front(
+      Transform back(
          eulerAngleRO(XYZ, vec3(0,0,0)),
-         vec3(-80,0,0),
+         vec3(0,0,0.5),
          vec3(1)
       );
 
-      setColor(vec3(0.4, 0.4, 0.8));
-      push();
-      transform(B * bodyGeometry);
-      drawSphere(vec3(0), 1);
-      pop();
+      Transform front(
+         eulerAngleRO(XYZ, vec3(0,0,0)),
+         vec3(-0.3,0,-0.5),
+         vec3(1.5)
+      );
 
-      setColor(vec3(0.8, 0, 0.0));
-      push();
-      transform(LT * lwingGeometry);
-      drawSphere(vec3(0), 1);
-      pop();
+      Transform flip(
+         eulerAngleRO(XYZ, vec3(0,0,0)),
+         vec3(0),
+         vec3(-1,-1,1)
+      );
 
-      setColor(vec3(0, 0.8, 0.0));
+      Transform bounce(
+         eulerAngleRO(XYZ, vec3(0,0,0)),
+         vec3(0,0,sin(elapsedTime()) * 5),
+         vec3(1)
+      );
+
       push();
-      transform(RT * rwingGeometry);
+      transform(B);
+         setColor(vec3(1,0,0));
+         drawCube(vec3(0),vec3(300,5,5));
+         setColor(vec3(0,1,0));
+         drawCube(vec3(0),vec3(5,300,5));
+         setColor(vec3(0,0,1));
+         drawCube(vec3(0),vec3(5,5,300));
+      transform(bodyGeometry);
+      setColor(vec3(0, 0, 0));
       drawSphere(vec3(0), 1);
       pop();
+      setColor(vec3(0.2, 0.2, 0.8));
+      push();
+         transform(LT * lwingGeometry);
+         push();
+            transform(back);
+            drawSphere(vec3(0), 1);
+            pop();
+         push();
+            transform(front);
+            drawSphere(vec3(0), 1);
+            pop();
+         pop();
+      setColor(vec3(0.2, 0.2, 0.8));
+      push();
+         transform(RT * rwingGeometry * flip);
+         push();
+            transform(back);
+            drawSphere(vec3(0), 1);
+            pop();
+         push();
+            transform(front);
+            drawSphere(vec3(0), 1);
+            pop();
+         pop();
    }
 
 private:
